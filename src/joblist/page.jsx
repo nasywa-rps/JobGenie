917c500df147d349b63d22e52535db2e586414d5
nasywarps <nasywarahmadhaniputrisetiawan@mail.ugm.ac.id>
2025-05-29 22:56:10 +0700

    fix: joblist page

--- src/joblist/page.jsx
+++ src/joblist/page.jsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
-import '../app.css';
+import '../App.css';
 
 const JobList = () => {
   const navigate = useNavigate();
