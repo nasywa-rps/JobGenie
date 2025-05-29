7a7ba750b7228a2f74d451c99439180cacbf86f2
nasywarps <nasywarahmadhaniputrisetiawan@mail.ugm.ac.id>
2025-05-30 01:02:29 +0700

    feat: integrate cloudinary to store files

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -19,6 +19,7 @@ export const register = async (req, res) => {
 
 export const login = async (req, res) => {
   const { username, password } = req.body;
+  console.log("body:", req.body);
   try {
     const user = await User.findOne({ username });
     if (!user) return res.status(400).json({ message: 'Invalid credentials' });
--- server/routes/searchRoutes.js
+++ server/routes/searchRoutes.js
@@ -1,16 +1,27 @@
 import express from 'express';
 import multer from 'multer';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import cloudinary from '../cloudinary.js';
 import { searchJob } from '../controllers/searchControllers.js';
 
 const router = express.Router();
 
-const storage = multer.diskStorage({
+/*const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, 'uploads/'),
   filename: (req, file, cb) => {
     const ext = file.originalname.split('.').pop();
     const filename = `${Date.now()}-${file.originalname}`;
     cb(null, filename);
   }
+});*/
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: 'jobgenie-cv',
+    allowed_formats: ['pdf', 'doc', 'docx'],
+    public_id: (req, file) => `${Date.now()}-${file.originalname}`,
+  },
 });
 
 const fileFilter = (req, file, cb) => {
@@ -21,7 +32,6 @@ const fileFilter = (req, file, cb) => {
 
 const upload = multer({
   storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
   fileFilter
 });
 
